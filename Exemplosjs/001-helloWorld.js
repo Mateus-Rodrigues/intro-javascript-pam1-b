d9e9baea7b7c88d81ec7b9ae1077604a5d0c34b0
Mateus_Rodrigues <59971705+Mateus-Rodrigues@users.noreply.github.com>
2023-02-13 22:50:16 -0300

    aula introduçao
    Criação de variáveis concatenação

---
+++ Exemplosjs/001-helloWorld.js
@@ -0,0 +1,29 @@
+// comentario de uma linha
+console.log("estou desenvolvendo em java-script");
+
+/*
+comentario de mais 
+de uma linha
+*/
+
+let nome = 'Mateus';
+let idade = 29;
+let trabalha = true;
+let altura_pessoa = 1.64;
+let alturaPessoa = 1.64;
+let coresFavoritas = ['Laranja', 'Verde', 'Preto',"Branco", 'Azul']; //arrey
+let endereco = {
+    logradouro: 'Rua',
+    nmlog: 'Basil',
+    num: 1200,
+    bairro: 'parque dos Anturios',
+}; //objeto
+
+let covid = null;
+let dengue;
+
+console.log(altura_pessoa);
+console.log('Nome: ' +nome);
+console.log(' O ' +nome+ ' Tem ' +idade+ ' Anos. ');
+
+console.log(typeof(coresFavoritas));
